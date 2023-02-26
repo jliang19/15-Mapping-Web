ea1933a7ca81dca8dadded072bab96c73f2a4b66
mr19th <zyl6842@gmail.com>
2023-02-26 16:02:43 -0500

    fixed errors

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -11,9 +11,11 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     for(var i=0; i<data.features.length;i++){
         var earthquake = data.features[i]
         var location = earthquake.geometry.coordinates
-        L.circle([location[1],location[0]],{
-            radius:70, color:"red"
-        }).addTo(earthquakeLayer)
+        var circle = L.circle([location[1],location[0]],{
+            color:getColor(location[2]*4)
+        })
+        circle.setRadius (location[2]*1500)
+        circle.addTo(earthquakeLayer)
        // earthquakes.push(L.circleMarker(L.latLng(data.features[i].geometry.coordinates[1],data.features[i].geometry.coordinates[0])),{
         //   radius:10
         //})
@@ -22,3 +24,32 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     console.log(earthquakeLayer)
     earthquakeLayer.addTo(map)
 })
+var legend = L.control({
+    position:"bottomright"
+})
+
+function getColor(magnitude){
+    console.log (magnitude)
+    if (magnitude>60) return "green"
+    if (magnitude>50) return "yellow"
+    if (magnitude>40) return "orange"
+    if (magnitude>30) return "red"
+    return "blue"
+}
+
+legend.onAdd=()=>{
+    let div = L.DomUtil.create("div")
+    const levels = [
+        0,30, 40, 50, 60
+    ]
+    var colors = [
+        "blue", "red", "orange", "yellow", "green"
+    ]
+    levels.forEach((level, i)=>{
+div.innerHTML+=" <div style = 'background:"+colors[i]+";height:10px;width:10px;'></div>"+" "+level + "<br>"
+console.log(level)
+    })
+    console.log(div)
+    return div
+}
+legend.addTo(map)
